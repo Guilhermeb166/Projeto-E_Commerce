1695bf12437b8533a906d3acdc1244859196ff2f
Guilherme <guilhermebarroso166@gmail.com>
2024-11-04 16:17:11 -0300

    snackbar criada "produto adicionado ao carrinho"

--- src/Components/Pages/Products/Cards/ProductCard.jsx
+++ src/Components/Pages/Products/Cards/ProductCard.jsx
@@ -1,16 +1,27 @@
 import propTypes from 'prop-types';
 import formatCurrency from '../../../patterns/FormatCurrency';
-import { useContext } from 'react';
+import React, { useContext,useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsFillCartPlusFill } from 'react-icons/bs';
 import Context from '../../../../Context/Context';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import Grow from '@mui/material/Grow';
 import styles from './ProductCard.module.css';
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function ProductCard({ data,imgClass }) {
     const { name, price, image } = data;
     const { setSelectedProduct, addToCart } = useContext(Context); // Pegando a função addToCart
+    const [openSnackbar, setOpenSnackbar] = useState(false);
     const {showCartBtn} = useContext(Context)
     const navigate = useNavigate();
+
+    function GrowTransition(props) {
+        return <Grow {...props} />;
+      }
 
     const handleCardClick = () => {
         setSelectedProduct(data)
@@ -22,6 +33,7 @@ export default function ProductCard({ data,imgClass }) {
     // Função para adicionar o produto ao carrinho
     const handleAddToCart = (e) => {
         e.stopPropagation(); // Evita que o clique no botão navegue para a página do produto
+        setOpenSnackbar(true); // Mostrar Snackbar ao adicionar ao carrinho
         addToCart(data); 
     };
 
@@ -29,8 +41,22 @@ export default function ProductCard({ data,imgClass }) {
         showCartBtn()
     }
 
+    const handleCloseSnackbar = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpenSnackbar(false);
+    };
     return (
         <div className={styles.cardProduct} onClick={handleCardClick}>
+            {/* Snackbar Component */}
+            <Snackbar open={openSnackbar} autoHideDuration={3000} onClose={handleCloseSnackbar}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            TransitionComponent={GrowTransition}>
+                <Alert onClose={handleCloseSnackbar} severity="success">
+                    Produto adicionado ao carrinho!
+                </Alert>
+            </Snackbar>
             <div className={styles.productImgWrapper}>
                 <img src={image} alt={name} className={imgClass} />
             </div>
@@ -46,6 +72,8 @@ export default function ProductCard({ data,imgClass }) {
                 <h2 className={styles.productPrice}>{formatCurrency(price, 'BRL')} </h2>
                 <button className={`${styles.addCartBtn} ${handleCartBtn ? styles.showBtn : ''}`} onClick={handleAddToCart}>Adicionar ao carrinho</button>
             </div>
+
+            
         </div>
     );
 }
