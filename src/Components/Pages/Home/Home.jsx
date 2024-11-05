56a84e1e4777f0005d481dbe33bc80fc3552ec91
Guilherme <guilhermebarroso166@gmail.com>
2024-11-05 20:03:13 -0300

    loading adicionado ao site

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import notebooks from '../../../productsList/Notebooks'
 import phones from '../../../productsList/Phones'
 import derivados from '../../../productsList/Derivados'
 import ProductCard from '../Products/Cards/ProductCard'
-
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 import Carousel from '../../Layout/Header/Carousel'
 import { useEffect, useState } from 'react'
 import getProductImgClass from '../../patterns/ReusableFunctions'
@@ -12,6 +12,7 @@ import getProductImgClass from '../../patterns/ReusableFunctions'
 export default function Home() {
     const [displayCount,setDisplayCount] = useState(4)
     const [numberCards,setNumberCards] = useState(16)
+    const [loading,setLoading] = useState(true)
 
    // Seleciona todos os notebooks e celulares
    const allProducts = [...notebooks.slice(0, 8), ...phones.slice(0, 8),...derivados.slice(0,8)];
@@ -26,6 +27,26 @@ export default function Home() {
     //pegar os 8 primeiros
     const notes = notebooks.slice(0, 4)
     const cells = phones.slice(0, 4)
+    // Embaralha os produtos e seleciona os primeiros 8
+    const shuffledProducts = shuffle(allProducts).slice(0, numberCards);
+
+    // Monitora o carregamento de imagens
+    useEffect(() => {
+        const images = [
+            ...notes.map(item => item.image),
+            ...cells.map(item => item.image),
+            ...shuffledProducts.map(item => item.image)
+        ];
+
+        const loadImage = src => new Promise((resolve) => {
+            const img = new Image();
+            img.src = src;
+            img.onload = resolve;
+        });
+
+        // Carrega todas as imagens e atualiza o estado ao finalizar
+        Promise.all(images.map(loadImage)).then(() => setLoading(false));
+    }, [notes, cells, shuffledProducts]);
 
     //useEffect para monitorar a largura da tela
     useEffect(()=>{
@@ -45,41 +66,43 @@ export default function Home() {
         return () => window.removeEventListener('resize', handleResize); //Limpa o listener ao desmontar
     },[])
 
-    // Embaralha os produtos e seleciona os primeiros 8
-   const shuffledProducts = shuffle(allProducts).slice(0, numberCards);
+   
     return (
         <div>
-            <Carousel/>
-            <main className={styles.home}>
-                <div className={styles.Card} >
-                    <h1 className={styles.cardTitle}>Notebooks</h1>
-                    <div className={styles.noteCard} >
-                        {notes.slice(0,displayCount).map(notebook => (
-                            <img key={notebook.id} src={notebook.image} alt={notebook.name} className={styles.noteImg} draggable='false'/>
-                        ))}
-                    </div>
-            
+            {loading ? (
+                <div className={styles.preloader}>
+                    <AiOutlineLoading3Quarters className={styles.loadingIcon} />
                 </div>
-                <div className={styles.Card}>
-                    <h1 className={styles.cardTitle}>Celulares</h1>
-                    <div className={styles.cellCard}>
-                        {cells.slice(0,displayCount).map(phone => (
-                            <img key={phone.id} src={phone.image} alt={phone.name} className={styles.phoneImg} draggable='false'/>
-                        ))}
-                    </div>
-                </div>
-            
-                    {/* Cards de produtos abaixo das seções de imagens */}
-                <div className={styles.productCardsSection}>
-                    <div className={styles.cardGrid}>
-                       
-                        {shuffledProducts.map(product => (
-                            <ProductCard key={product.id} data={product} imgClass={`${styles.productImage} ${getProductImgClass(product)}`}/>
-                        ))}
-                    </div>
-               </div>
-             
-            </main>
+            ) : (
+                <>
+                    <Carousel />
+                    <main className={styles.home}>
+                        <div className={styles.Card}>
+                            <h1 className={styles.cardTitle}>Notebooks</h1>
+                            <div className={styles.noteCard}>
+                                {notes.slice(0, displayCount).map(notebook => (
+                                    <img key={notebook.id} src={notebook.image} alt={notebook.name} className={styles.noteImg} draggable='false' />
+                                ))}
+                            </div>
+                        </div>
+                        <div className={styles.Card}>
+                            <h1 className={styles.cardTitle}>Celulares</h1>
+                            <div className={styles.cellCard}>
+                                {cells.slice(0, displayCount).map(phone => (
+                                    <img key={phone.id} src={phone.image} alt={phone.name} className={styles.phoneImg} draggable='false' />
+                                ))}
+                            </div>
+                        </div>
+                        <div className={styles.productCardsSection}>
+                            <div className={styles.cardGrid}>
+                                {shuffledProducts.map(product => (
+                                    <ProductCard key={product.id} data={product} imgClass={`${styles.productImage} ${getProductImgClass(product)}`} />
+                                ))}
+                            </div>
+                        </div>
+                    </main>
+                </>
+            )}
         </div>
-    )
+    );
 }
