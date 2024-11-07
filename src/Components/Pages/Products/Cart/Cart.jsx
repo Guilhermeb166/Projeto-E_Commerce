f81bad7155081941e0ae92bdbbaf219d9262c5ee
Guilherme <guilhermebarroso166@gmail.com>
2024-11-07 16:47:33 -0300

    2  versões para tablet na tela de cart

--- src/Components/Pages/Products/Cart/Cart.jsx
+++ src/Components/Pages/Products/Cart/Cart.jsx
@@ -77,7 +77,7 @@ export default function Cart() {
   // Atualiza o showTable quando a tela é redimensionada
   useEffect(() => {
     const handleResize = () => {
-      const shouldShowTable = window.innerWidth >= 481;
+      const shouldShowTable = window.innerWidth >= 800;
       setShowTable(shouldShowTable);
     };
 
